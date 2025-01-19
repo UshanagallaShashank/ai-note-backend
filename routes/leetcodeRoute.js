bbd71f7e27d64aa7fa43021fef88df3cadf6e817
Ushanagalla Shashank <shashank.u1312@gmail.com>
2025-01-20 00:07:23 +0530

    update leetcode route

--- routes/leetcodeRoute.js
+++ routes/leetcodeRoute.js
@@ -179,7 +179,7 @@ router.post('/singleproblem', async (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Captured Element Screenshot</title>
+      <title>${problemDetails.title}</title>
       <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
       <style>
        body {
@@ -324,7 +324,7 @@ router.get('/problemwithslug/:title_slug', async (req, res) => {
      <head>
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title>Captured Element Screenshot</title>
+       <title>${problemDetails.title}</title>
        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
        <style>
         body {
